7aa27b63aae1232385b25e72f479cf9671e3324c
pee-que-elle <pql@cock.li>
2018-06-17 14:00:29 +0200

    Add support for adding dangerous functions dynamically

--- src/argparser.py
+++ src/argparser.py
@@ -3,18 +3,21 @@ def parse():
     import json
     import os.path
 
+    openabs = lambda x: open(os.path.join(os.path.dirname(__file__), x))
+
     options = {
         "mode": "directory",    
         "project-directory": "./",
         "recursive": True,
-        "filename-matches": ".+",
-        "language-associations": json.load(open(os.path.join(os.path.dirname(__file__), "../cfg/filetype_regex.json")))
+        "filename-pattern": ".+",
+        "language-associations": json.load(openabs("../cfg/filetype_regex.json")),
+        "dangerous-functions": json.load(openabs("../cfg/dangerous_functions.json"))
     }
     
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--mode", 
+    parser.add_argument("--mode", "-m",
                         help="Mode. Directory traverses a whole directory (recursive by default, see --no-recursive) while file analyzes a single file",
                         action="store",
                         choices=["directory", "file"],
@@ -32,24 +35,62 @@ def parse():
                         action="store_false",
                         dest="recursive")
     
-    parser.add_argument("--filename-matches", "-m",
+    parser.add_argument("--filename-pattern", "-p",
                         help="Only analyze files matching this regex. Defaults to .+",
                         action="store",
                         default=".+",
-                        dest="filename-matches")
+                        dest="filename-pattern")
     
-    parser.add_argument("--language-associations", "-a",
+    parser.add_argument("--language-associations", "-l",
                         help="A JSON object in the form of {\"language:\": \"regex\", \"language2\": \"regex2\", ...} to associate arbitrary filenames with arbitrary languages if for example a non-standard extension is used",
                         type=json.loads,
                         default="{}",
                         dest="language-associations")
+    
+    parser.add_argument("--language-associations-file",
+                        help="--language-associations, but loads it from a file instead of an argument",
+                        type=lambda x: json.load(open(x)) if x != "" else {},
+                        default="",
+                        dest="language-associations-2")
+
+    parser.add_argument("--dangerous-functions", "-f",
+                        help="A JSON object in the form of {\"language\": [\"functionname\", \"functionname2\"], \"language2\": [\"functionname3\", \"functionname4\"]} to add project-specific dangerous functions to languages",
+                        type=json.loads,
+                        default="{}",
+                        dest="dangerous-functions")
+    
+    parser.add_argument("--dangerous-functions-file",
+                        help="--dangerous-functions, but loads it from a file instead of an argument",
+                        type=lambda x: json.load(open(x)) if x != "" else {},
+                        default="",
+                        dest="dangerous-functions-2")
 
     parsed = vars(parser.parse_args())
+    def merge(x):
+        parsed[x] = {**parsed[x+"-2"], **parsed[x]}
+        del parsed[x+"-2"]
+    
+    merge("language-associations")
+    merge("dangerous-functions")
 
     # Join dicts instead of overwriting
-    key = "language-associations"
-    options[key] = {**options[key], **parsed[key]}
-    del parsed[key]
+    
+    def merge_option_dict(key):
+        options[key] = {**options[key], **parsed[key]}
+        del parsed[key]
+
+    merge_option_dict("language-associations")
+    
+
+    def merge_lists(key):
+        for lang in parsed[key]:
+            if lang in options[key]:
+                options[key][lang].extend(parsed[key][lang])
+                options[key][lang] = list(set(options[key][lang]))
+
+        del parsed[key]
+
+    merge_lists("dangerous-functions")
 
     return {**options, **parsed }
 
